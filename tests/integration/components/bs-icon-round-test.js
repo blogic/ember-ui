e8ca68898d4a90e5b4987ac4509d14f6a3edfed7
Chris Manson <chris@manson.ie>
2025-05-15 10:30:00 +0100

    fix round icons

--- tests/integration/components/bs-icon-round-test.js
+++ tests/integration/components/bs-icon-round-test.js
@@ -10,7 +10,9 @@ module('Integration | Component | bs-icon-round', function (hooks) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<BsIconRound />`);
+    await render(hbs`<BsIconRound @name="wifi" />`);
+
+    // await this.pauseTest();
 
     assert.dom().hasText('');
 
